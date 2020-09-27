471c8b78776e094cbffa5e996e47c92d254fe1ad
mchung-code <tkd.m.chung@gmail.com>
2020-09-27 17:10:01 -0600

    Add accidentally untracked files

--- src/main/matlab/SampleNuclearReactor.m
+++ src/main/matlab/SampleNuclearReactor.m
@@ -2,19 +2,35 @@ classdef SampleNuclearReactor
     properties
         half_life {mustBeNumeric} % half_life of the radioactive source
         base_run_path
+        simulink_run_path
+        beta {mustBeNumeric}
+        lambda {mustBeNumeric}
     end
     
     methods
-       function obj = SampleNuclearReactor(half_life)
+       function obj = SampleNuclearReactor(half_life, beta, lambda)
            obj.half_life = half_life;
            obj.base_run_path = fileparts(pwd); 
+           obj.simulink_run_path = strcat(obj.base_run_path, '\simulink\');
+           obj.beta = beta; % effective delayed neutron fraction
+           obj.lambda = lambda; % decay constant of the delayed neutron group
        end
        
-       function RadioActiveDecay(obj)
-           simulink_run_path = strcat(string(obj.base_run_path), '\simulink\radioactive_decay.slx');
-           sim(simulink_run_path)
-           
+       function [decay_time, decay_activity] = RadioActiveDecay(obj)
+           decay_run_path = strcat(obj.simulink_run_path, 'radioactive_decay.slx');
+           sim(decay_run_path); 
+           decay_time = tout; % decay_time in years
+           decay_activity = activity; % Decay activity
        end
+       
+       function [conc_time, concentration] = PointKineticEquation(obj)
+           % point kinetic equation based on treatise by Hetrick
+           point_kin_run_path = strcat(obj.simulink_run_path, 'point_kinetic_equation.slx');
+           sim(point_kin_run_path); 
+           conc_time = tout;
+           concentration = conc;
+           
+       end 
    end
        
 end
--- src/main/matlab/reactor_main.m
+++ src/main/matlab/reactor_main.m
@@ -1,8 +1,19 @@
-function reactor_main 
-    import pkg.*
-    a = SampleNuclearReactor(5.27);
-    a.RadioActiveDecay()
-end 
+import pkg.*
+% parameters of the delayed neutron group
+half_life = 5.27;
+beta = 0.006502; % effective delayed neutron fraction
+lambda = 0.07669; % decay constant
+
+reactor = SampleNuclearReactor(half_life, beta, lambda);
+
+% for radioactive decay
+[decay_time, decay_activity] = reactor.RadioActiveDecay();
+
+% for point kinetic equation
+[conc_time, concentration] = reactor.PointKineticEquation();
+
+
+
 
 
 
