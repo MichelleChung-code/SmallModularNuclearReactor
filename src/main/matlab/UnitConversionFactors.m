a92dff5ec6d969c43cb7f3db933b654264149c3c
mchung-code <tkd.m.chung@gmail.com>
2020-10-20 12:24:57 -0600

    Add comment on naming convention

--- src/main/matlab/UnitConversionFactors.m
+++ src/main/matlab/UnitConversionFactors.m
@@ -1,1 +1,4 @@
 % File of Constants to run prior to running main script
+% Naming convention should be x_over_y i.e. m to nm conversion would be
+% m_over_nm = 1/1e9 such that converting from nm to m would just involve
+% multiplying this factor
