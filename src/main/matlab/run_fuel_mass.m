1c583c1c969c3b7bb62f8a4e019eae311f39ebbc
mchung-code <tkd.m.chung@gmail.com>
2020-10-20 12:46:50 -0600

    Added Overall Heat Transfer Coefficient Calculation

--- src/main/matlab/run_fuel_mass.m
+++
@@ -1,7 +0,0 @@
-close all, clear
-clc
-% Just to call functions
-
-fuelMass = fuel_mass() % in kg
-
-% Returns: 9.7432e+04 kg
---
+++ src/main/matlab/run_prep_data.m
@@ -0,0 +1,10 @@
+close all, clear
+clc
+% Just to call functions
+
+fuelMass = fuel_mass() % in kg
+Ufo = reactor_core_Ufo() % in W/(m^2*K)
+
+% Returns: 
+% Fuel Mass: 9.7432e+04 kg
+% Ufo: 7.8595e+04 W/(m^2*K)
