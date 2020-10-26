adcae10449bb2571ae8db62f93f3a12f199813ff
Marcelo Mathias <marcelo.mathias02@gmail.com>
2020-10-26 13:19:07 -0600

    Adding the neutron kinetics for the middle nodes as well as reactivity function

--- src/main/matlab/NeutronKinetics.m
+++ src/main/matlab/NeutronKinetics.m
@@ -6,6 +6,9 @@ classdef NeutronKinetics
         beta {mustBeNumeric}
         lambda {mustBeNumeric}
         N {mustBeNumeric}
+        alpha_fuel {mustBeNumeric}
+        alpha_moderator {mustBeNumeric}
+        alpha_reflector {mustBeNumeric}
     end
     
     methods
@@ -19,6 +22,52 @@ classdef NeutronKinetics
            % Dyanmic inputs that can change
            obj.coupling_coeffs_matrix = coupling_coeffs_matrix;
            obj.N = N; % number of nodes
+           
+           obj.alpha_fuel = alpha_fuel; 
+           obj.alpha_moderator = alpha_moderator;
+           obj.alpha_reflector = alpha_reflector;
+       end
+       
+       function rho = reactivity(obj, Tc, Tr)
+           % DOCUMENT WHAT EACH VALUE REPRESENT
+           % Tc = Temperature of the fuel element
+           % Tr = Temperature of the reflector
+           % Tc0, tr0 = initial temperature of fuel elements and reflector 
+           % rho_control_rods = reactivity introduced by the control rods
+           
+           rho_control_rods = 1.8e-3; % Need to find a valid number
+           Tc0 = 1000; %Change number
+           Tr0 = 1000; %Change number
+           rho = rho_control_rods + (obj.alpha_fuel + obj.alpha_moderator)*(Tc - Tc0) + obj.alpha_reflector*(Tr -Tr0);
+       
+       end
+       
+       function dxdt = relative_neutron_flux_middle_node(obj, t, x, node)
+           % DOCUMENT WHAT EACH VALUE REPRESENTS
+           % x(1) = ni neutron flux of the ith node
+           % x(2:7) = Concentrations of delayed groups for ith node
+           
+           n0 = 1 % ni-1 neutron flux of the ith-1 node
+           n2 = 2 % ni+1 neutron flux of the ith+1 node
+           rho_i = 1 % Reactivity of ith node 
+           
+           % Relative neutron flux for ith node
+           dnidt_term1 = (rho_i - obj.beta - obj.coupling_coeffs_matrix(node,node))*(1/obj.lambda)*x(1);
+           dnidt_term2 = (1/obj.lambda)*(obj.coupling_coeffs_matrix(node, node-1)*n0 + obj.coupling_coeff_matrix(node, node+1)*n2);
+           dnidt_term3 = obj.sum_beta_concentration_over_lambda(x(2:7), obj.lambda);
+           dxdt(1) = dnidt_term1 + dnidt_term2 + dnidt_term3;
+           
+           % Delayed Neutron Group Concentrations for Node i
+           % TODO - see if there is a nicer way to do this - I hate how
+           % this is done right now but solving the ODE45 inside the ODE45
+           % was causing issues
+           dxdt(2) = obj.lambda_ls_delayed_groups(1)*(x(1)-x(2)); 
+           dxdt(3) = obj.lambda_ls_delayed_groups(2)*(x(1)-x(3));
+           dxdt(4) = obj.lambda_ls_delayed_groups(3)*(x(1)-x(4));
+           dxdt(5) = obj.lambda_ls_delayed_groups(4)*(x(1)-x(5));
+           dxdt(6) = obj.lambda_ls_delayed_groups(5)*(x(1)-x(6));
+           dxdt(7) = obj.lambda_ls_delayed_groups(6)*(x(1)-x(7));    
+           
        end
        
        function dxdt = relative_neutron_flux_first_node(obj, t, x) 
