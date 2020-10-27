f90026e3ed2c6d2afd9feb0a0f346db486907325
Marcelo Mathias <marcelo.mathias02@gmail.com>
2020-10-27 13:16:36 -0600

    Adding Initial Value functions

--- src/main/matlab/NeutronKineticsRun.m
+++ src/main/matlab/NeutronKineticsRun.m
@@ -14,11 +14,14 @@ coupling_coeffs_matrix = [3.5 7.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
 
 % for the initial set up of the numerical integration 
 % think about how to set up such that nodes are dynamic
-tspan = [0 2]';
+tspan = [0 1]';
 
 % TODO need to get the actual initial conditions
 N = 10; % number of nodes
-x0 = ones(N*10+5, 1);
+%x0 = ones(N*10+5, 1);
+from_csv = readtable('Initial Values.csv');
+csv_array = table2array(from_csv);
+x0 = csv_array;
 neutron_kinetics = NeutronKinetics(coupling_coeffs_matrix, N);
 [tout, x] = neutron_kinetics.solve_neutron_kinetics(tspan, x0);
 
