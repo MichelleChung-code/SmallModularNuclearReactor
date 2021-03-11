26990b5961fa16bd68ec183b218b8384f9aebf26
mchung-code <tkd.m.chung@gmail.com>
2021-03-10 17:09:59 -0700

    Write base code

--- src/main/python/heat_exchanger/HeatExchanger.py
+++ src/main/python/heat_exchanger/HeatExchanger.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class HeatExchanger:
     """
     For validation of Symmetry heat exchanger
@@ -5,7 +8,7 @@ class HeatExchanger:
     Use NTU method
     """
 
-    def __init__(self, mass_flow_hot, mass_flow_cold, Cp_hot, Cp_cold, Tin_hot, Tin_cold, U, A):
+    def __init__(self, mass_flow_hot, mass_flow_cold, Cp_hot, Cp_cold, Tin_hot, Tin_cold, U, A, flow_arrangement):
         self.mass_flow_hot = mass_flow_hot
         self.mass_flow_cold = mass_flow_cold
         self.Cp_hot = Cp_hot
@@ -14,6 +17,7 @@ class HeatExchanger:
         self.Tin_cold = Tin_cold
         self.U = U
         self.A = A
+        self.flow_arr = flow_arrangement
 
     @staticmethod
     def size_heat_exhanger(heat_duty, heat_transfer_coeff, Tin_hot, Tout_hot, Tin_cold, Tout_cold, tube_OD,
@@ -27,11 +31,47 @@ class HeatExchanger:
         min_C, max_C = min([C_hot, C_cold]), max([C_hot, C_cold])
         rel_C = min_C / max_C
 
-    def effectiveness_ntu(self):
-        pass
+        NTU = (self.U * self.A) / min_C
+
+        # get effectiveness
+        epsilon = self.effectiveness_ntu(NTU, rel_C)
+
+        max_heat_flow = min_C * (self.Tin_hot - self.Tin_cold)
+        eff_heat_flow = epsilon * max_heat_flow
+
+        Tout_hot = self.Tin_hot - eff_heat_flow / C_hot
+        Tout_cold = self.Tin_cold + eff_heat_flow / C_cold
+
+        return {'Tout_hot': Tout_hot,
+                'Tout_cold': Tout_cold}
+
+    def effectiveness_ntu(self, NTU, rel_C, N=1):
+        """
+
+        Args:
+            type:
+            rel_C:
+            N: <int> number of passes.  Defaults to 1.
+
+        Returns:
+            epsilon: <float> effectiveness of heat exchanger
+        """
+
+        # https://en.wikipedia.org/wiki/NTU_method
+        type_dict = {'Parallel_Flow': lambda NTU, rel_C: (1 - np.exp(-NTU * (1 + rel_C))) / (1 + rel_C),
+                     'CounterCurrent_Flow': lambda NTU, rel_C: (1 - np.exp(-NTU * (1 - rel_C))) / (
+                             1 - rel_C * np.exp(-NTU * (1 - rel_C)))}
+
+        epsilon = type_dict.get(self.flow_arr, lambda NTU, rel_C: exec('raise(Exception(x))'))(NTU, rel_C)
+
+        # assert that effectiveness is between 0 and 1
+        assert 0 <= epsilon <= 1
+        return epsilon
 
     def __call__(self):
-        self.NTU_method()
+        res_dict = self.NTU_method()
+
+        print(res_dict)
 
 
 # https://www.pdhonline.com/courses/m371/m371content.pdf
@@ -47,5 +87,21 @@ def prelim_area_guess(unit_name):
 
 
 if __name__ == '__main__':
-    x = HeatExchanger()
+    A = prelim_area_guess('SMR_Steam_Generator')
+
+    # INPUTS - UPDATE THESE
+    mass_flow_hot = None  # kg/s
+    mass_flow_cold = None  # kg/s
+    Cp_hot = None  # KJ/kg-K
+    Cp_cold = None  # KJ/kg-K
+    Tin_hot = None  # K
+    Tin_cold = None  # K
+    U = None  # kW/m2.K
+    A = None  # m2
+    flow_arrangement = 'CounterCurrent_Flow'
+
+    x = HeatExchanger(mass_flow_hot, mass_flow_cold, Cp_hot, Cp_cold, Tin_hot, Tin_cold, U, A, flow_arrangement)
     x()
+
+    # todo calculate the heat duty and LMTD for sizing
+
